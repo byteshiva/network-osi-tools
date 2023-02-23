d0c0d779b61a768f2037bf927fdc9e3abd2679d1
Siva <byteshiva@users.noreply.github.com>
2023-02-23 18:35:47 +0530

    Add script to sniff Android device on the same network
    Add a Python script that uses Scapy to sniff network traffic from an Android device on the same network. The script sets up a filter for the Android device's IP address and prints statistics about the packets received. 
    
    Note: Make sure to update the <Android_Device_IP> in the code to the IP address of the Android machine you want to sniff.

---
+++ network_traffic_analysis/android_sniffer/android_sniffer.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+from scapy.all import *
+
+def process_packet(packet, stats):
+    proto = packet[IP].proto
+    if proto not in stats['packets_by_protocol']:
+        stats['packets_by_protocol'][proto] = 0
+    stats['packets_by_protocol'][proto] += 1
+
+    src = packet[IP].src
+    if src not in stats['packets_by_src_ip']:
+        stats['packets_by_src_ip'][src] = 0
+    stats['packets_by_src_ip'][src] += 1
+
+def print_stats(stats):
+    print('---Packet Statistics---')
+    for proto, count in stats['packets_by_protocol'].items():
+        print(f"Protocol {proto}: {count} packets")
+    for ip, count in stats['packets_by_src_ip'].items():
+        print(f"Source IP {ip}: {count} packets")
+
+def main():
+    stats = defaultdict(lambda: 0, {'packets_by_protocol': {}, 'packets_by_src_ip': {}})
+    filter_packets = 'ip and host <Android_Device_IP>'
+    packets = sniff(filter=filter_packets, prn=lambda x: process_packet(x, stats))
+    print_stats(stats)
+
+if __name__ == '__main__':
+    main()
